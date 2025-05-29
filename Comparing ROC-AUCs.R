06ab21ddbd73c0d45197a1aab00a57518b3841be
magnuslohne <magnuslohne00@outlook.com>
2025-05-29 12:30:26 +0200

    Update and rename Comparing ROC-AUCs.Rmd to Comparing ROC-AUCs.R

--- Comparing ROC-AUCs.R
+++ Comparing ROC-AUCs.R
@@ -1,7 +1,3 @@
----
-title: "Comparing ROC-AUCs"
----
-
 models <- list(
   XGB = roc(actual_class_test, test_prob_pred_XGB),
   NN = roc(actual_class_test, test_prob_pred_NN),
@@ -10,10 +6,12 @@ models <- list(
   SVM = roc(actual_class_test, test_prob_pred_SVM)
 )
 
-compare_auc <- combn(names(models), 2, simplify = FALSE, FUN = function(pair) {
+compare_auc(models) <- combn(names(models), 2, simplify = FALSE, FUN = function(pair) {
   delongtest <- roc.test(models[[pair[1]]], models[[pair[2]]], method = "delong", alternative = "greater")
   if (delongtest$p.value > 0.5) {
     delongtest <- roc.test(models[[pair[1]]], models[[pair[2]]], method = "delong", alternative = "less")
   }
   c(model1 = pair[1], model2 = pair[2], alternative_hypothesis = delongtest$alternative, p_value = delongtest$p.value)
 })
+
+rocauc_test_mat <- do.call(rbind, compare_auc)
